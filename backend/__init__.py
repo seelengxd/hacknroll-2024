7c9bb0cba159ebca6eb48cce7fcb75ac88428834
see leng <seelengxd@gmail.com>
2024-01-20 12:40:34 +0800

    feat: init backend

---
+++ backend/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+from .models.db import db, add_db_setup_commands
+
+from .blueprints.items import ItemList
+from flask_restful import Api
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        SQLALCHEMY_DATABASE_URI="postgresql+psycopg2://postgres:postgres@localhost:5432/todos")
+
+    db.init_app(app)
+    add_db_setup_commands(app)
+
+    api = Api(app)
+
+    api.add_resource(ItemList, "/items")
+
+    return app
---
+++ backend/models/db.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+import click
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+def add_db_setup_commands(app: Flask):
+    def init_db():
+        # todo: init models later
+        # from . import models later
+        with app.app_context():
+            db.create_all()
+
+    @app.cli.command("init-db")
+    def init_db_command():
+        init_db()
+        click.echo("Initialised the database.")
